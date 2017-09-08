13eb087d71b6e9e04495c84eb447382508cc2c8e
kumarkd <kumarkd@BANKUMARKD1-M>
2017-09-08 21:40:42 +0530

    Adding GCMonTrigger

---
+++ GCMonTrigger/GCMonTrigger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace GCMonTrigger
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Process.Start(ConfigurationSettings.AppSettings["GCMonAppPath"]);
+        }
+    }
+}
